dff5b11edc7a51af073a44795023328c4c610e8b
Tino Fiumara <tjfiumara@gmail.com>
2021-03-04 06:48:48 -0500

    :wq first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Open and Close the dropdown menu
+function menuClick(){
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("showcase-img");
+var i;
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+for (i = 0; i < img.length; i++) {
+    img[i].onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
